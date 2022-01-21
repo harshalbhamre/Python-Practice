8678a182fe5ff7ff08d15184d5a85d6f3f0db438
harshalbhamre <66110697+harshalbhamre@users.noreply.github.com>
2022-01-21 17:32:05 +0530

    Create Quiz 1(for loop).py

---
+++ Quiz/Quiz 1(for loop).py
@@ -0,0 +1,9 @@
+list1 = ["Harry", 3, "Larry", 1, "Marie", 10, "sarry", 25, 26, 5, 7 ,99, 69, 420]
+
+for item in list1 :
+
+  if type(item) ==int and item > 6 :
+
+    print("items are : ", item)
+
+    
